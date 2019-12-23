ffd3e8788b8609b2a05b8c540936fac164c7fb82
Petter Eriksson <ipetterik@gmail.com>
2019-12-23 11:48:30 -0800

    Uses a single field for buffering items instead of an array

--- src/jvm/clojure/lang/XFSeqStep.java
+++ src/jvm/clojure/lang/XFSeqStep.java
@@ -9,8 +9,9 @@ public class XFSeqStep extends AFn {
     //       Setting it to 8 makes it faster on map inc and filter even
     //       compared to vanilla lazy-seqs
     private static final int MIN_SIZE = 8;
+    private static final Object NOTHING = new Object();
 
-    private Object[] arr;
+    private Object item;
     private int idx;
 
     private final IFn xf;
@@ -18,156 +19,78 @@ public class XFSeqStep extends AFn {
 
     public XFSeqStep(IFn xf, ISeq s) {
         this.idx = 0;
-        this.arr = new Object[MIN_SIZE];
+        this.item = NOTHING;
         this.s = s;
 
         this.xf = (IFn)xf.invoke(this);
     }
 
-    private ISeq invokeChunked(IChunkedSeq cs) {
-        IChunk ch = cs.chunkedFirst();
-        if (arr == null) {
-            int size = ch.count();
-            arr = new Object[size < MIN_SIZE ? MIN_SIZE : size];
-        }
-        if (this == ch.reduce(xf, this)) {
-            return cs.chunkedMore();
-        } else {
-            return PersistentList.EMPTY;
-        }
-    }
-
     public Object invoke() {
-        ISeq c = s.seq();
-        if (c == null) {
-            if (arr == null) {
-                arr = new Object[idx < MIN_SIZE ? MIN_SIZE : idx];
-            }
+        ISeq ret;
+        ISeq s = this.s.seq();
+        if (s == null) {
             xf.invoke(this);
-            if (idx == 0) {
-                return null;
-            } else {
-                // Doesn't need to set arr to nil as this is the final step in XFSeq.NextStep
-                return new ChunkedCons(new ArrayChunk(arr, 0, idx), null);
+            if (item == NOTHING)
+                ret = null;
+            else {
+                ret = new Cons(item, null);
             }
         } else {
-            if (c instanceof IChunkedSeq) {
-                c = invokeChunked((IChunkedSeq) c);
-            } else {
-                if (arr == null) {
-                    arr = new Object[idx < MIN_SIZE ? MIN_SIZE : idx];
+            if (s instanceof IChunkedSeq) {
+                IChunkedSeq cs = (IChunkedSeq)s;
+                IChunk ch = cs.chunkedFirst();
+                ISeq more = cs.chunkedMore();
+
+                int size = ch.count();
+                Object[] arr = new Object[size];
+                int pos = 0;
+                for (int i = 0; i < size; i++) {
+                    Object invoked = xf.invoke(this, ch.nth(i));
+                    if (item != NOTHING) {
+                        arr[pos++] = item;
+                        item = NOTHING;
+                    }
+
+                    // checking for reduced.
+                    if (this != invoked) {
+                        more = PersistentList.EMPTY;
+                        break;
+                    }
                 }
-                if (this == xf.invoke(this, c.first())) {
-                    c = c.more();
+                this.s = more;
+                if (pos == 0) {
+                    ret = new LazySeq(this);
                 } else {
-                    c = PersistentList.EMPTY;
+                    ret = new ChunkedCons(new ArrayChunk(arr, 0, pos), new LazySeq(this));
+                }
+            } else {
+                ISeq more;
+                if (this == xf.invoke(this, s.first())) {
+                    more = s.more();
+                } else {
+                    more = PersistentList.EMPTY;
+                }
+                this.s = more;
+
+                if (item == NOTHING) {
+                    ret = new LazySeq(this);
+                } else {
+                    ret = new Cons(item, new LazySeq(this));
+                    item = NOTHING;
                 }
             }
-            s = c;
-            return toSeq(new LazySeq(this));
         }
+        return ret;
     }
 
     @Override
-    public Object invoke(Object arg1) {
-        return arg1;
+    public Object invoke(Object a) {
+        return a;
     }
 
     @Override
     public Object invoke(Object a, Object b) {
-        if (idx == arr.length) {
-            // Grows quickly to 32, then slows down.
-            // 8 * 4 * 2 * 2 * 2
-            Object[] larger = new Object[idx * (idx <= 8 ? 4 : 2)];
-            System.arraycopy(arr, 0, larger, 0, idx);
-            arr = larger;
-        }
-
-        arr[idx++] = b;
+        item = b;
         return a;
-    }
-
-    private Object toSeq(ISeq more) {
-        ISeq s;
-        switch(idx) {
-            case 0:
-                s = more;
-                break;
-            // TODO: Verify whether handrolling these arities is a good idea.
-            case 1:
-                s = new Cons(arr[0], more);
-                idx = 0;
-                arr[0] = null;
-                break;
-            case 2:
-                s = new Cons(arr[0], new Cons(arr[1], more));
-                idx = 0;
-                arr[0] = null;
-                arr[1] = null;
-                break;
-            case 3:
-                s = new Cons(arr[0], new Cons(arr[1], new Cons(arr[2], more)));
-                idx = 0;
-                arr[0] = null;
-                arr[1] = null;
-                arr[2] = null;
-                break;
-            case 4:
-                s = new Cons(arr[0], new Cons(arr[1], new Cons(arr[2], new Cons(arr[3], more))));
-                idx = 0;
-                arr[0] = null;
-                arr[1] = null;
-                arr[2] = null;
-                arr[3] = null;
-                break;
-            case 5:
-            case 6:
-            case 7:
-            case 8:
-            case 9:
-            case 10:
-            case 11:
-            case 12:
-            case 13:
-            case 14:
-            case 15:
-            case 16:
-            case 17:
-            case 18:
-            case 19:
-            case 20:
-            case 21:
-            case 22:
-            case 23:
-            case 24:
-            case 25:
-            case 26:
-            case 27:
-            case 28:
-            case 29:
-            case 30:
-            case 31:
-            case 32:
-                s = new ChunkedCons(new ArrayChunk(arr, 0, idx), more);
-                arr = null;
-                idx = 0;
-                break;
-            default:
-                // Returns 32 sized chunks in case the transduction created
-                // more items than that. When chained, it can blow up.
-                // This problem was found with code that repeated interpose:
-                // (interpose nil (interpose nil ... (interpose nil (range)) ... ))
-                s = more;
-                int offset = idx;
-                do {
-                    int end = offset;
-                    offset = Math.max(0, offset - 32);
-                    s = new ChunkedCons(new ArrayChunk(arr, offset, end), s);
-                } while (offset > 0);
-                idx = 0;
-                arr = null;
-                break;
-        }
-        return s;
     }
 }
