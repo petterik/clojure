8ade04773ed300d717b134cf0fc6802c5a5d2074
Petter Eriksson <ipetterik@gmail.com>
2019-12-23 20:45:49 -0800

    Using ArrayCons instead of ChunkedCons + ArrayChunk

--- src/jvm/clojure/lang/XFSeqStep.java
+++ src/jvm/clojure/lang/XFSeqStep.java
@@ -63,14 +63,8 @@ public class XFSeqStep extends AFn {
         if (idx == 0) {
             return null;
         } else {
-            return new ChunkedCons(new ArrayChunk(arr, 0, idx), null);
+            return new ArrayCons(arr, 0, idx, null);
         }
-    }
-
-    private Object[] arrayCopy() {
-        Object[] ret = new Object[idx];
-        System.arraycopy(arr, 0, ret, 0, idx);
-        return ret;
     }
 
     @Override
@@ -91,7 +85,6 @@ public class XFSeqStep extends AFn {
             // TODO: Verify whether handrolling some cases is a good idea.
             case 1:
                 seq = new Cons(arr[0], seq);
-                arr[0] = null;
                 idx = 0;
                 break;
             case 2:
@@ -125,9 +118,7 @@ public class XFSeqStep extends AFn {
             case 30:
             case 31:
             case 32:
-                Object[] ret = new Object[idx];
-                System.arraycopy(arr, 0, ret, 0, idx);
-                seq = new ChunkedCons(new ArrayChunk(ret, 0, idx), seq);
+                seq = new ArrayCons(arrayCopy(), 0, idx, seq);
                 // TODO: Do we need to clear this "cache/buffer" for GC purposes?
                 System.arraycopy(NULLS, 0, arr, 0, idx);
                 idx = 0;
@@ -139,6 +130,12 @@ public class XFSeqStep extends AFn {
         return seq;
     }
 
+    private Object[] arrayCopy() {
+        Object[] ret = new Object[idx];
+        System.arraycopy(arr, 0, ret, 0, idx);
+        return ret;
+    }
+
     private ISeq chunkLargeResult(ISeq s) {
         // Returns 32 sized chunks in case the transduction created
         // more items than that. When chained, it can blow up.
@@ -148,10 +145,119 @@ public class XFSeqStep extends AFn {
         do {
             int end = offset;
             offset = Math.max(0, offset - 32);
-            s = new ChunkedCons(new ArrayChunk(arr, offset, end), s);
+            s = new ArrayCons(arr, offset, end, s);
         } while (offset > 0);
         arr = new Object[32];
         idx = 0;
         return s;
+    }
+
+
+    public static class ArrayCons extends ASeq implements IChunkedSeq, IChunk {
+
+        private final Object[] array;
+        private final int offset;
+        private final int end;
+        private final ISeq more;
+
+        public ArrayCons(IPersistentMap meta, Object[] array, int offset, int end, ISeq more) {
+            super(meta);
+            this.array = array;
+            this.offset = offset;
+            this.end = end;
+            this.more = more;
+        }
+
+        public ArrayCons(Object[] array, int offset, int end, ISeq more) {
+            super(null);
+            this.array = array;
+            this.offset = offset;
+            this.end = end;
+            this.more = more;
+        }
+
+        @Override
+        public IChunk chunkedFirst() {
+            return this;
+        }
+
+        @Override
+        public ISeq chunkedNext() {
+            return chunkedMore().seq();
+        }
+
+        @Override
+        public ISeq chunkedMore() {
+            return more == null ? PersistentList.EMPTY : more;
+        }
+
+        @Override
+        public Object first() {
+            return array[offset];
+        }
+
+        @Override
+        public ISeq next() {
+            return more().seq();
+        }
+
+        @Override
+        public ISeq more() {
+            int next = offset + 1;
+            if (next == end) {
+                return more == null ? PersistentList.EMPTY : more;
+            } else {
+                return new ArrayCons(array, next, end, more);
+            }
+        }
+
+        @Override
+        public Obj withMeta(IPersistentMap meta){
+            if(meta != _meta)
+                return new ArrayCons(meta, array, offset, end, more);
+            return this;
+        }
+
+        // IChunk
+
+        @Override
+        public IChunk dropFirst() {
+            if(offset == end)
+                throw new IllegalStateException("dropFirst of empty chunk");
+            return new ArrayCons(array, offset + 1, end, more);
+        }
+
+        @Override
+        public Object reduce(IFn f, Object start) {
+            Object ret = f.invoke(start, array[offset]);
+            if (RT.isReduced(ret))
+                return ret;
+            for (int i = offset + 1; i < end; i++) {
+                ret = f.invoke(start, array[i]);
+                if (RT.isReduced(ret)) {
+                    return ret;
+                }
+            }
+            return ret;
+        }
+
+        @Override
+        public Object nth(int i) {
+            return array[offset + i];
+        }
+
+        @Override
+        public Object nth(int i, Object notFound) {
+            if (i >= 0 && i < count()) {
+                return array[offset + i];
+            } else {
+                return notFound;
+            }
+        }
+
+        @Override
+        public int count() {
+            return end - offset;
+        }
     }
 }
